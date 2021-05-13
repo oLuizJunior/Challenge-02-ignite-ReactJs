333c3a30d552960a20d1842728a00c434d718a23
Junior Rodrigues <junior.luiz182@gmail.com>
2021-05-12 21:43:38 -0300

    Criando componentes

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+
+import './styles/global.scss';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  
+  const state = {
+    selectedGenreId,
+    setSelectedGenreId
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar state={state} />
+      <Content state={state} />
+    </div>
+  )
+}
